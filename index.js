6b6588cf5db017f43a905adfefe582c35ef7dcc2
unknown <ajoy9532@gmail.com>
2023-06-05 11:52:26 +0600

    fixed some problem

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
     const taskCollection = client.db("taskTaker").collection("tasks");
 
     // get all tasks api
